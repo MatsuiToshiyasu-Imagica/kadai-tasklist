e84e17d964905c9492fb38d7adc9b43b87398d66
toshiya <toshiya.magna0923@gmail.com>
2019-06-12 02:21:49 +0000

    db retake

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,3 @@
 class Task < ApplicationRecord
- #validates :content, presence: true, length: { maximum: 10 }
- validates :status, presence: true, length: { maximum: 10 }
- validates :content, presence: true, length: { maximum: 255 }
+    belongs_to :user
 end
